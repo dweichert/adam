af8acc34a45e9aa36bd3a056458db9de5863df62
David Weichert <info@davidweichert.de>
2016-03-20 11:58:48 +0100

    Adds enable calling app_dev.php explicitly using server environment variables.
    Sets environment vars ADAM_ENV and ADAM_DEBUG in dev and prod Ansible Nginx configuration.

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,23 +8,29 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-//if (isset($_SERVER['HTTP_CLIENT_IP'])
-//    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-//    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-//) {
-//    header('HTTP/1.0 403 Forbidden');
-//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-//}
+if (isset($_SERVER['ADAM_ENV']) && 'dev' !== $_SERVER['ADAM_ENV'])
+{
+    header('HTTP/1.0 403 Forbidden');
+    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
  */
 $loader = require __DIR__.'/../app/autoload.php';
-Debug::enable();
 
-$kernel = new AppKernel('dev', true);
+$debug = false;
+if (isset($_SERVER['ADAM_DEBUG']) && 'true' === $_SERVER['ADAM_DEBUG'])
+{
+    $kernel = new AppKernel('dev', true);
+    Debug::enable();
+}
+else
+{
+    $kernel = new AppKernel('dev', false);
+}
+
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
